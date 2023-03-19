ce6cbe568e9829ab604e91b487c12d9ca264ceeb
Paulo Sánchez <dev.szblzpaulo@gmail.com>
2023-03-19 17:32:27 +0100

    Add external path handling

--- src/modules/storage.py
+++ src/modules/storage.py
@@ -8,16 +8,16 @@ import json
 from typing import Any, Optional
 
 
-def load_json(name: str) -> dict[Any, Any]:
+def load_json(path: str) -> dict[Any, Any]:
     """Load a JSON file in memory.
 
     Args:
-        name (str): path to the file.
+        path (str): path to the file.
 
     Returns:
         dict[Any, Any]: parsed dictionary.
     """
-    with open(name, mode="r", encoding="utf-8") as fp:
+    with open(path, mode="r", encoding="utf-8") as fp:
         return json.load(fp)
 
 
@@ -28,10 +28,15 @@ class DBHandler:
     hashes and linked data.
     """
 
-    def __init__(self) -> None:
-        """Initialize a DBHandler instance."""
-        self._by_id = self._load("src/databases/by_id.json")
-        self._by_hash = self._load("src/databases/by_hash.json")
+    def __init__(self, path: str) -> None:
+        """Initialize a DBHandler instance.
+
+        Args:
+            path (str): path to the database directory.
+        """
+        self.path = path
+        self._by_id = self._load(f"{path}/by_id.json")
+        self._by_hash = self._load(f"{path}/by_hash.json")
 
     def _load(self, name: str) -> dict[Any, Any]:
         """Load a JSON file in memory.
