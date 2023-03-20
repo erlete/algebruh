b8fa00af0500512fa28d666507ac55a921d7c7ab
Paulo Sánchez <dev.szblzpaulo@gmail.com>
2023-03-20 17:35:55 +0100

    Code style update (#1)
    * Update docstring
    
    * Update imports
    
    * Update name and structure
    
    Updated class name and structure.
    
    * Implement a custom answer field
    
    * Redefine parent class
    
    * Update window structure process
    
    Distributed window configuration instructions into several methods to make them easier to test and debug.
    
    * Update clear methods and rename attributes
    
    Updated several clear methods so that the code flow is more understandable and less repetitive.
    
    Also renamed some attributes.
    
    * Format `AnswerWindow` class
    
    Formatted the rest of the `AnswerWindow` class and renamed it.
    
    * Fix path

--- src/modules/gui.py
+++ src/modules/gui.py
@@ -12,6 +12,7 @@ import os
 import sys
 from io import BytesIO
 from random import randint
+from typing import Any, Dict
 
 import numpy as np
 import PyQt6
@@ -19,41 +20,48 @@ from PIL import Image
 from PIL.ImageQt import ImageQt
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QIcon, QPixmap
-from PyQt6.QtWidgets import (QApplication, QLabel, QLineEdit, QMainWindow,
-                             QPushButton, QVBoxLayout, QWidget)
+from PyQt6.QtWidgets import (QApplication, QHBoxLayout, QLabel, QLineEdit,
+                             QMainWindow, QPushButton, QScrollArea, QTextEdit,
+                             QVBoxLayout, QWidget)
 
 from .connection import Session
 from .storage import DBHandler
 
 
-def resource_path(relative_path):
-    """Get absolute path to resource, works for dev and for PyInstaller."""
+def resource_path(relative_path: str) -> str:
+    """Get absolute path to resource (required by .exe filesystem).
+
+    Args:
+        relative_path (str): path to the resource.
+
+    Returns:
+        str: parsed path to the resource.
+    """
     try:
-        base_path = sys._MEIPASS
+        base_path = sys._MEIPASS  # type: ignore
     except Exception:
         base_path = os.path.abspath(".")
 
     return os.path.join(base_path, relative_path)
 
 
-class ImageLabel(QLabel):
-    """Label for the image to display."""
+class ImageField(QLabel):
+    """Image field that allows drag and drop operations."""
 
     def __init__(self):
         """Initialize an ImageLabel instance."""
         super().__init__()
 
+        self.setMinimumSize(200, 100)
+
         self.setAcceptDrops(True)
-        self.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.setIndent(4)
-        self.setLineWidth(4)
-        self.setMargin(10)
-        self.setMidLineWidth(4)
         self.setOpenExternalLinks(True)
-        self.setText("\n\nDrag and drop image here\n\n")
+        self.setWordWrap(True)
+
+        self.setText("Drag and drop image here")
         self.setTextFormat(Qt.TextFormat.RichText)
         self.setTextInteractionFlags(Qt.TextInteractionFlag.NoTextInteraction)
-        self.setWordWrap(True)
+        self.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.setStyleSheet("""
             QLabel {
@@ -61,8 +69,80 @@ class ImageLabel(QLabel):
             }
         """)
 
+    def clear(self):
+        """Clear image field contents."""
+        super().clear()
+        self.setText("Drag and drop image here")
+        self.setStyleSheet("""
+            QLabel {
+                border: 4px dashed #aaa
+            }
+        """)
 
-class AnswerWidget(QWidget):
+
+class AnswerField(QScrollArea):
+    """Scrollable text field for the answer.
+
+    Attributes:
+        text (QTextEdit): the text field.
+    """
+
+    def __init__(self, placeholder_text: str, read_only: bool = True) -> None:
+        """Initialize an AnswerField instance.
+
+        Args:
+            placeholder_text (str): the placeholder text.
+            read_only (bool): whether the field is read-only.
+        """
+        super().__init__()
+
+        self._placeholder_text = placeholder_text
+
+        self._text = QTextEdit()
+        self._text.setReadOnly(read_only)
+        self._text.setContentsMargins(5, 5, 5, 5)
+        self._text.setPlaceholderText(placeholder_text)
+        self._text.setLineWrapMode(QTextEdit.LineWrapMode.NoWrap)
+
+        self.setWidgetResizable(True)
+        self.setMinimumSize(200, 100)
+
+        self.setAlignment(Qt.AlignmentFlag.AlignLeft)
+
+        self.setWidget(self._text)
+
+    @property
+    def text(self) -> str:
+        """Get the text field.
+
+        Returns:
+            str: the text field.
+        """
+        return self._text.toPlainText()
+
+    @text.setter
+    def text(self, value: str) -> None:
+        """Set the text field.
+
+        Args:
+            value (str): the text field.
+        """
+        if not isinstance(value, str):
+            raise TypeError(
+                "expected type str for"
+                + f" {self.__class__.__name__}.text but got"
+                + f" {type(value).__name__} instead"
+            )
+
+        self._text.setText(value)
+
+    def clear(self):
+        """Clear the answer field."""
+        self._text.clear()
+        self._text.setPlaceholderText(self._placeholder_text)
+
+
+class AnswerWindow(QMainWindow):
     """Answer retrieval widget.
 
     Attributes:
@@ -80,55 +160,60 @@ class AnswerWidget(QWidget):
             session (Session): client session.
             admin_login(bool): whether the login is performed by an admin.
         """
+        super().__init__()
+
         self.session = session
         self.dbhandler = DBHandler(resource_path("databases"))
+
         self.__admin_login = admin_login
-        self.__log = {}
-
-        super().__init__()
-        self.resize(1200, 400)
-
-        self.setAcceptDrops(True)
+        self.__log: Dict[str, Any] = {}
 
         self.setWindowTitle("Algebruh - Answer Retrieval")
         self.setWindowIcon(QIcon(resource_path("icon.ico")))
 
-        mainLayout = QVBoxLayout()
+        self.setAcceptDrops(True)
+        self.setMinimumSize(800, 300)
 
-        self.photoViewer = ImageLabel()
+        self.setup()
 
-        self.clearButton = QPushButton("Clear")
-        self.clearButton.clicked.connect(self.clear_pixmap)
+    def setup(self) -> None:
+        """Set up window components."""
+        self.setup_widgets()
+        self.setup_event_handlers()
+        self.setup_layout()
 
-        self.answerLabel = QLabel("Answer: ")
-        self.answerLabel.setWordWrap(True)
-        self.answerLabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.answerLabel.setStyleSheet("""
-            QLabel {
-                font-weight: bold;
-                font-size: 13px;
-            }
-        """)
+    def setup_widgets(self) -> None:
+        """Set up window widgets."""
+        self.image_field = ImageField()
+        self.answer_field = AnswerField("Answer: ")
+        self.explanation_field = AnswerField("Explanation: ")
+        self.clear_button = QPushButton("Clear")
 
-        self.explanationLabel = QLabel("Explanation: ")
-        self.explanationLabel.setWordWrap(True)
-        self.explanationLabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.explanationLabel.setStyleSheet("""
-            QLabel {
-                font-size: 11px;
-            }
-        """)
+    def setup_event_handlers(self) -> None:
+        """Set up widget event handlers."""
+        self.clear_button.clicked.connect(self.reset)
 
-        mainLayout.addWidget(self.photoViewer)
-        mainLayout.addWidget(self.answerLabel)
-        mainLayout.addWidget(self.explanationLabel)
-        mainLayout.addWidget(self.clearButton)
+    def setup_layout(self) -> None:
+        """Set up window layout."""
+        central_wdg, secondary_wdg = QWidget(), QWidget()
+        central_lay, secondary_lay = QVBoxLayout(), QHBoxLayout()
 
-        self.setLayout(mainLayout)
+        secondary_lay.addWidget(self.answer_field)
+        secondary_lay.addWidget(self.explanation_field)
+        secondary_wdg.setLayout(secondary_lay)
+
+        central_lay.addWidget(self.image_field)
+        central_lay.addWidget(secondary_wdg)
+        central_lay.addWidget(self.clear_button)
+        central_wdg.setLayout(central_lay)
+
+        self.setCentralWidget(central_wdg)
 
     @staticmethod
     def invert(answer: bool) -> str:
         """Invert an answer.
+
+        What is this method? Why is it here? Who knows...
 
         Args:
             answer (bool): answer to invert.
@@ -138,6 +223,7 @@ class AnswerWidget(QWidget):
         """
         if answer == "Verdadero":
             return "Falso"
+
         elif answer == "Falso":
             return "Verdadero"
 
@@ -155,17 +241,11 @@ class AnswerWidget(QWidget):
         """
         return path.endswith(cls.VALID_EXTENSIONS) or path.startswith("http")
 
-    def clear_pixmap(self) -> None:
-        """Clear the pixel map from the window."""
-        self.photoViewer.clear()
-        self.photoViewer.setText("\n\nDrag and drop image here\n\n")
-        self.photoViewer.setStyleSheet("""
-            QLabel {
-                border: 4px dashed #aaa
-            }
-        """)
-        self.answerLabel.setText("Answer: ")
-        self.explanationLabel.setText("Explanation: ")
+    def reset(self) -> None:
+        """Reset contents of all fields."""
+        self.image_field.clear()
+        self.answer_field.clear()
+        self.explanation_field.clear()
 
     def dragEnterEvent(self, event: PyQt6.QtGui.QDragEnterEvent) -> None:
         """Handle the drag enter event.
@@ -174,14 +254,14 @@ class AnswerWidget(QWidget):
             event (PyQt6.QtGui.QDragEnterEvent): drag enter event.
         """
         if self.is_valid(event.mimeData().text()):
-            self.photoViewer.setStyleSheet("""
+            self.image_field.setStyleSheet("""
                 QLabel {
                     border: 6px dashed #9fe05e
                 }
             """)
 
         else:
-            self.photoViewer.setStyleSheet("""
+            self.image_field.setStyleSheet("""
                 QLabel {
                     border: 6px dashed #940c13
                 }
@@ -195,7 +275,7 @@ class AnswerWidget(QWidget):
         Args:
             event (PyQt6.QtGui.QDragLeaveEvent): drag leave event.
         """
-        self.photoViewer.setStyleSheet("""
+        self.image_field.setStyleSheet("""
             QLabel {
                 border: 4px dashed #aaa
             }
@@ -215,56 +295,74 @@ class AnswerWidget(QWidget):
         if not self.session.is_logged_in():
             raise RuntimeError("the client is not logged into the site.")
 
-        if self.is_valid(event.mimeData().text()):
+        if not self.is_valid(event.mimeData().text()):
+            event.ignore()
+            self.reset()
 
-            url = event.mimeData().urls()[0].url()
+            return None
 
-            self.session.browser.open(url)
-            response = self.session.browser.response()
+        event.accept()
 
-            img = Image.open(BytesIO(response.get_data()))
-            img_hash = str(hash(tuple(np.array(img.getdata()).flatten())))
+        self.session.browser.open(event.mimeData().urls()[0].url())
+        response = self.session.browser.response()
+        img = Image.open(BytesIO(response.get_data()))
+        img_hash = str(hash(tuple(np.array(img.getdata()).flatten())))
+        data = self.dbhandler.hash_search(img_hash)
 
-            data = self.dbhandler.hash_search(img_hash)
-            if data is None:
-                self.answerLabel.setText("Answer: Not found")
-                self.explanationLabel.setText("Explanation: Not found")
-            else:
-                if img_hash in self.__log:
-                    self.answerLabel.setText(
-                        f"Answer: {self.__log[img_hash]['answer']}"
-                    )
-                    self.explanationLabel.setText(
-                        f"Explanation: {self.__log[img_hash]['explanation']}"
-                    )
+        self.image_field.setPixmap(QPixmap.fromImage(ImageQt(img)))
 
-                else:
-                    if self.__admin_login:
-                        ans = data["answer"]
-                        exp = data["explanation"]
-                    else:
-                        wrong = not bool(randint(0, 7))
-                        if wrong:
-                            ans = self.invert(data["answer"])
-                            exp = ""
-                        else:
-                            ans = data["answer"]
-                            exp = data["explanation"]
+        if data is None:
+            self.answer_field.text = "Answer not found!"
+            self.explanation_field.text = "Explanation not found!"
 
-                    self.answerLabel.setText(f"Answer: {ans}")
-                    self.explanationLabel.setText(f"Explanation: {exp}")
-                    self.__log[img_hash] = {
-                        "answer": ans,
-                        "explanation": exp
-                    }
+            return None
 
-            self.photoViewer.setPixmap(QPixmap.fromImage(ImageQt(img)))
+        if img_hash in self.__log:
+            self.answer_field.text = (
+                f"Answer: {self.__log[img_hash]['answer']}"
+            )
+            self.explanation_field.text = (
+                f"Explanation: {self.__log[img_hash]['explanation']}"
+            )
 
-            event.accept()
+            return None
+
+        # Wait what? You found the easter egg!
+
+        if self.__admin_login:
+            ans = data["answer"]
+            exp = data["explanation"]
 
         else:
-            event.ignore()
-            self.clear_pixmap()
+            # Yes, the program fails questions on purpose.
+            #   This is done in order to avoid excessive OP tool usage.
+            #   Yes, yes, you want to use it in admin mode too... I get it.
+            #   Just add a % before your email in the login and you will
+            #   be authenticated as admin.
+            #
+            # Example (user vs. admin login)
+            #   john@doe.com
+            #   %john@doe.com
+            #
+            # You tell this to anyone and I will filter your IP to the
+            #   russian mafia.
+
+            wrong = not bool(randint(0, 7))
+
+            if wrong:
+                ans = self.invert(data["answer"])  # Fail on purpose >:)
+                exp = ""
+            else:
+                ans = data["answer"]
+                exp = data["explanation"]
+
+        self.answer_field.text = f"Answer: {ans}"
+        self.explanation_field.text = f"Explanation: {exp}"
+
+        self.__log[img_hash] = {
+            "answer": ans,
+            "explanation": exp
+        }
 
 
 class LoginWindow(QMainWindow):
@@ -282,6 +380,16 @@ class LoginWindow(QMainWindow):
         self.resize(400, 300)
         self.setFixedSize(self.size())
 
+        self.setup()
+
+    def setup(self) -> None:
+        """Set up window widgets."""
+        self.setup_widgets()
+        self.setup_event_handlers()
+        self.setup_layout()
+
+    def setup_widgets(self) -> None:
+        """Set up window widgets."""
         self.header = QLabel("Welcome to Algebruh!")
         self.header.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.header.setStyleSheet("""
@@ -291,11 +399,11 @@ class LoginWindow(QMainWindow):
             }
         """)
 
-        self.login_status = QLabel(
-            "Login to continue." +
-            "\nDo not worry, we do not store your credentials." +
-            "\nThey are sent only to the login page and then discarded."
-        )
+        self.login_status = QLabel("""
+            Login to continue.
+            Do not worry, we do not store your credentials.
+            They are sent only to the login page and then discarded.
+        """.replace("\t", ""))
         self.login_status.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.login_status.setStyleSheet("""
             QLabel {
@@ -306,6 +414,7 @@ class LoginWindow(QMainWindow):
 
         self.user = QLineEdit()
         self.user.setPlaceholderText("Username")
+
         self.password = QLineEdit()
         self.password.setPlaceholderText("Password")
         self.password.setEchoMode(QLineEdit.EchoMode.Password)
@@ -323,23 +432,28 @@ class LoginWindow(QMainWindow):
             }
         """)
 
-        self._layout = QVBoxLayout()
-        self._layout.setContentsMargins(20, 20, 20, 20)
-        self._layout.setSpacing(10)
-
-        self._layout.addWidget(self.header)
-        self._layout.addWidget(self.login_status)
-        self._layout.addWidget(self.user)
-        self._layout.addWidget(self.password)
-        self._layout.addWidget(self.submit)
-        self._layout.addWidget(self.copyrightLabel)
-
-        widget = QWidget()
-        widget.setLayout(self._layout)
-
-        self.setCentralWidget(widget)
-
+    def setup_event_handlers(self) -> None:
+        """Set up widget event handlers."""
         self.submit.clicked.connect(self.login)
+
+    def setup_layout(self) -> None:
+        """Set up window layout."""
+        central_wdg = QWidget()
+        central_lay = QVBoxLayout()
+
+        central_lay.setContentsMargins(20, 20, 20, 20)
+        central_lay.setSpacing(10)
+
+        central_lay.addWidget(self.header)
+        central_lay.addWidget(self.login_status)
+        central_lay.addWidget(self.user)
+        central_lay.addWidget(self.password)
+        central_lay.addWidget(self.submit)
+        central_lay.addWidget(self.copyrightLabel)
+
+        central_wdg.setLayout(central_lay)
+
+        self.setCentralWidget(central_wdg)
 
     def login(self) -> None:
         """Login method for the application.
@@ -354,6 +468,7 @@ class LoginWindow(QMainWindow):
         username = self.user.text()
         password = self.password.text()
 
+        # Huh... what could this be?
         admin_login = False
         if username.startswith("%"):
             admin_login = True
@@ -364,7 +479,7 @@ class LoginWindow(QMainWindow):
 
         if session.is_logged_in():
             self.close()
-            self.main = AnswerWidget(session, admin_login)
+            self.main = AnswerWindow(session, admin_login)
             self.main.show()
 
         else:
