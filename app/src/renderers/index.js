89aafa57f9a82aec7a3dbdcf12df2ff3cee57a25
Paulo Sánchez <dev.szblzpaulo@gmail.com>
2023-03-28 23:53:18 +0200

    WebApp implementation (#2)
    * Add Node exclusions
    
    * Add database
    
    Added question database for app queries.
    
    * Add app configuration files
    
    * Add media files
    
    * Add HTML pages
    
    Added all three pages that compose the application.
    
    * Add renderers
    
    Added renderer scripts that are linked to the HTML pages.
    
    * Add app entry point
    
    * Update documentation
    
    * Update database
    
    Added rendered text to database entries and also parsed other values for consistency.
    
    * Implement text rendering functionality
    
    Implemented image-to-text rendering functionality.
    
    * Fix answer counting issue
    
    * Update timer
    
    It's funny, do not judge me for it.
    
    * Solve formatting issues
    
    * Define constants
    
    * Update average grade
    
    * Update average grade
    
    Updated grade slogan.
    
    * Update navigation bars
    
    * Update database location
    
    * Fix drag and drop issues
    
    Fixed tab opening during drag and drop mistakes.
    
    Also updated drop behavior.
    
    * Update description

---
+++ app/src/renderers/index.js
@@ -0,0 +1,45 @@
+// Constants:
+const TOTAL_QUESTIONS_TAG = "total_questions";
+const ANSWERED_QUESTIONS_TAG = "answered_questions";
+const DATABASE_PATH = "databases/questions.json";
+
+/**
+ * Set the total number of questions in the database to the given tag.
+ * @date 3/28/2023 - 6:53:12 PM
+ *
+ * @async
+ * @param {string} tag_id - ID of the tag to set the total number of questions to.
+ * @param {string} db_path - Path to the database.
+ */
+async function setTotalQuestions(tag_id, db_path) {
+    const data = await (await fetch(db_path)).json();
+    const totalQuestions = document.getElementById(tag_id);
+
+    totalQuestions.innerHTML = Object.keys(data).length;
+}
+
+/**
+ * Set the total number of answered questions in the database to the given tag.
+ * @date 3/28/2023 - 6:53:26 PM
+ *
+ * @async
+ * @param {string} tag_id - ID of the tag to set the total number of answered questions to.
+ * @param {string} db_path - Path to the database.
+ */
+async function setAnsweredQuestions(tag_id, db_path) {
+    const data = await (await fetch(db_path)).json();
+    const answeredQuestions = document.getElementById(tag_id);
+    let count = 0;
+
+    for (const key in data) {
+        if (data[key].answer != null) {
+            count++;
+        }
+    }
+
+    answeredQuestions.innerHTML = count;
+}
+
+// Set up dynamic data for the page:
+setTotalQuestions(TOTAL_QUESTIONS_TAG, DATABASE_PATH);
+setAnsweredQuestions(ANSWERED_QUESTIONS_TAG, DATABASE_PATH);
