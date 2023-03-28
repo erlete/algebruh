58d7456c3f0650b9e188fea5d103aad72a09be18
Paulo Sánchez <dev.szblzpaulo@gmail.com>
2023-03-28 20:26:57 +0200

    Implement text rendering functionality
    Implemented image-to-text rendering functionality.

--- app/src/renderers/solver.js
+++ app/src/renderers/solver.js
@@ -16,8 +16,11 @@ async function setup() {
  */
 function dropEnter(event) {
     event.preventDefault();
-    document.getElementById("answer").innerHTML = "Drop the image to get the answer";
-    document.getElementById("explanation").innerHTML = "Drop the image to get the explanation";
+    document.getElementById("answer").innerHTML = "No image detected.";
+    document.getElementById("explanation").innerHTML = "No image detected.";
+
+    document.getElementById("text-render-div").style.display = "none";
+    document.getElementById("text-render-btn-div").style.display = "none";
 }
 
 /**
@@ -30,6 +33,9 @@ function dropLeave(event) {
     event.preventDefault();
     document.getElementById("answer").innerHTML = "No image detected.";
     document.getElementById("explanation").innerHTML = "No image detected.";
+
+    document.getElementById("text-render-div").style.display = "none";
+    document.getElementById("text-render-btn-div").style.display = "none";
 }
 
 /**
@@ -50,11 +56,19 @@ function drag(event) {
  */
 function drop(event) {
     event.preventDefault();
-    var data = event.dataTransfer.getData("text");
-    data = getFormattedData(sanitize(data));
+
+    var url = event.dataTransfer.getData("text");
+    var id = sanitize(url);
+    var data = getFormattedData(id);
 
     document.getElementById("answer").innerHTML = data.answer;
     document.getElementById("explanation").innerHTML = data.explanation;
+    document.getElementById("text-render").innerHTML = data.text;
+
+    if (data.answer != "No answer found.") {
+        document.getElementById("text-render-btn").style.display = "block";
+        document.getElementById("text-render-btn-div").style.display = "block";
+    }
 }
 
 /**
@@ -66,6 +80,7 @@ function drop(event) {
  */
 function sanitize(url) {
     var match = url.match(/download_[0]*(\d*)/);
+
     if (match) {
         return match[1];
     }
@@ -76,28 +91,46 @@ function sanitize(url) {
  * Format answer.
  * @date 3/28/2023 - 6:45:33 PM
  *
- * @param {string} answer - Answer to be formatted.
+ * @param {*} answer - Answer to be formatted.
  * @returns {string} - Formatted answer. "No answer found" if answer is empty.
  */
 function format_answer(answer) {
-    if (answer == "") {
-        return "No answer found";
+    if (answer == null || answer == undefined) {
+        return "No answer found.";
     }
-    return `<b>${answer}</b>`;
+    answer = answer.toString();
+    return `<b>${answer.charAt(0).toUpperCase()}${answer.substring(1)}</b>`;
 }
 
 /**
  * Format explanation.
  * @date 3/28/2023 - 6:45:33 PM
  *
- * @param {string} explanation - Explanation to be formatted.
+ * @param {*} explanation - Explanation to be formatted.
  * @returns {string} - Formatted explanation. "No explanation found" if explanation is empty.
  */
 function format_explanation(explanation) {
-    if (explanation == "") {
-        return "No explanation found";
+    if (explanation == null || explanation == undefined) {
+        return "No explanation found.";
     }
-    return `<b>${explanation}</b>`;
+    explanation = explanation.toString();
+    return `<b>${explanation.charAt(0).toUpperCase()}${explanation.substring(1)}</b>`;
+}
+
+
+/**
+ * Format text.
+ * @date 3/28/2023 - 7:39:29 PM
+ *
+ * @param {*} text - Text to be formatted.
+ * @returns {string} - Formatted text. "No text found" if text is empty.
+ */
+function format_text(text) {
+    if (text == null || text == undefined) {
+        return "No text found.";
+    }
+    text = text.toString();
+    return `<b>${text.charAt(0).toUpperCase()}${text.substring(1)}</b>`;
 }
 
 /**
@@ -111,14 +144,25 @@ function getFormattedData(key) {
     if (key in window.data) {
         return {
             "answer": format_answer(window.data[key].answer),
-            "explanation": format_explanation(window.data[key].explanation)
+            "explanation": format_explanation(window.data[key].explanation),
+            "text": window.data[key].text
         };
     }
     return {
-        "answer": format_answer(""),
-        "explanation": format_explanation("")
+        "answer": format_answer(null),
+        "explanation": format_explanation(null),
+        "text": format_text(null)
     };
 };
+
+function renderText() {
+    var timeout = document.getElementById("text-render").innerHTML.length * 10;
+
+    setTimeout(() => {
+        document.getElementById("text-render-div").style.display = "block";
+        document.getElementById("text-render-btn-div").style.display = "none";
+    }, timeout);
+}
 
 // Fetch data and store it in `window.data`:
 setup();
