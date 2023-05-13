ef893451935d5716a5d2f735125c06946bf1d835
Paulo Sánchez <dev.szblzpaulo@gmail.com>
2023-05-13 18:20:43 +0200

    Add text solver skeleton page

---
+++ src/renderers/text_solver.js
@@ -0,0 +1,10 @@
+// Constants:
+const DEFAULT_ANSWER = "No search text detected.";
+const DEFAULT_EXPLANATION = "No search text detected.";
+const DEFAULT_TEXT = "No search text detected.";
+
+const MISSING_ANSWER = "No answer found.";
+const MISSING_EXPLANATION = "No explanation found.";
+const MISSING_TEXT = "No text found.";
+
+const DATABASE_PATH = "databases/questions.json";
