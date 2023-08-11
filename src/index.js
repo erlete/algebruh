5ffb7ffda23a6276921eda3321c24409f1c2b520
Paulo Sánchez <dev.szblzpaulo@gmail.com>
2023-08-11 06:02:25 +0200

    Update spacing

--- src/index.js
+++ src/index.js
@@ -2,35 +2,35 @@ const { app, BrowserWindow } = require("electron");
 const path = require("path");
 
 if (require("electron-squirrel-startup")) {
-  app.quit();
+    app.quit();
 }
 
 // Main window:
 const createWindow = () => {
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
-    },
-    icon: path.join(__dirname, "media/icon.ico")
-  });
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+        },
+        icon: path.join(__dirname, "media/icon.ico")
+    });
 
-  mainWindow.maximize();
-  mainWindow.loadFile(path.join(__dirname, "index.html"));
+    mainWindow.maximize();
+    mainWindow.loadFile(path.join(__dirname, "index.html"));
 };
 
 // Event listeners:
 app.on("ready", createWindow);
 
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
 });
 
 app.on("activate", () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
 });
