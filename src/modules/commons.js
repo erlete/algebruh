6b6fdb57907840a053ec0cccf5aac369c673992f
Paulo Sánchez <dev.szblzpaulo@gmail.com>
2023-08-12 03:26:25 +0200

    Update documentation and fix minor issues
    Updated names and documentation for common methods.
    
    Also fixed a minor access issue within the `getBestMatch` method.

--- src/modules/commons.js
+++ src/modules/commons.js
@@ -1,57 +1,85 @@
 const DATABASE_PATH = "databases/questions.json";
 
-function updateConfidenceThreshold() {
-    const confidenceThreshold = document.getElementById("confidence-threshold").value;
-    document.getElementById("confidence-threshold-value").innerHTML = confidenceThreshold;
+// Data format functions:
 
-    // If there has been any previous input, filter the result:
-    if (lastInput !== null) {
-        const outputData = getFormattedData(lastInput, confidenceThreshold);
-
-        for (let key of KEYS) {
-            document.getElementById(key).innerHTML = outputData[key];
-        }
-    }
+/**
+ * Convert text to bold.
+ * @date 8/12/2023 - 3:09:39 AM
+ *
+ * @param {string} text - Text to convert.
+ * @returns {string} - Bold text.
+ */
+function bold(text) {
+    return text !== null && text !== undefined ? `<b>${text}</b>` : text;
 }
 
 /**
- * Get the best match for the given text and confidence threshold.
- * @date 8/11/2023 - 3:06:02 AM
+ * Convert boolean answer to readable text.
+ * @date 8/12/2023 - 3:10:14 AM
  *
- * @param {string} text
- * @param {number} confidenceThreshold
- * @returns {{ data: Object; confidence: string; }} - Best match data and confidence.
+ * @param {boolean} answer - Answer.
+ * @returns {string} - Formatted answer.
+ */
+function formatAnswer(answer) {
+    return bold(answer ? "Verdadero" : "Falso");
+}
+
+
+/**
+ * Convert explanation to bold or show missing message.
+ * @date 8/12/2023 - 3:11:18 AM
+ *
+ * @param {string} explanation - Explanation. {@link null} if missing.
+ * @returns {string} - Formatted explanation.
+ */
+function formatExplanation(explanation) {
+    return explanation !== null ? bold(explanation) : MISSING_MESSAGE;
+}
+
+// Pattern matching functions:
+
+/**
+ * Get the best match for the given text and confidence threshold.
+ * @date 8/12/2023 - 3:13:43 AM
+ *
+ * @param {string} text - Input text.
+ * @param {number} confidenceThreshold - Confidence threshold.
+ * @returns {{ text: any; confidence: string; answer: str | null; explanation: str | null; } | null} - Best match. {@link null} if no match.
  */
 function getBestMatch(text, confidenceThreshold) {
+    // Generate match array:
     let matchArray = Object.keys(window.data).map(key => ({
-        "id": key,
         "text": window.data[key].text,
-        "similarity": new difflib.SequenceMatcher(null, window.data[key].text, text).ratio()
+        "confidence": new difflib.SequenceMatcher(null, window.data[key].text, text).ratio(),
+        "answer": window.data[key].answer,
+        "explanation": window.data[key].explanation
     }));
 
     // Filter by confidence threshold and sort from higher to lower ratio:
-    matchArray = matchArray.filter(match => match.similarity >= confidenceThreshold / 100);
-    matchArray.sort((a, b) => b.similarity - a.similarity);
+    matchArray = matchArray.filter(match => match.confidence >= confidenceThreshold / 100);
+    matchArray.sort((a, b) => b.confidence - a.confidence);
 
+    // Return best match if available, else return null:
     return matchArray.length === 0 ? null : {
         "text": matchArray[0].text,
-        "confidence": `${Math.round(matchArray[0].similarity * 10000) / 100}%`,
-        "answer": window.data[matchArray[0].id].answer,
-        "explanation": window.data[matchArray[0].id].explanation
+        "confidence": `${Math.round(matchArray[0].confidence * 10000) / 100}%`,
+        "answer": matchArray[0].answer,
+        "explanation": matchArray[0].explanation
     };
 }
 
 /**
- * Format form data.
- * @date 8/11/2023 - 5:22:08 AM
+ * Get formatter data for the given text and confidence threshold.
+ * @date 8/12/2023 - 3:16:24 AM
  *
  * @param {string} text - Input text.
  * @param {number} confidenceThreshold - Confidence threshold.
- * @returns {{ answer: string; explanation: string; match: string; confidence: string; }} - Formatted data.
+ * @returns {{ text: string; match: string; confidence: string; answer: string; explanation: string; }} - Formatted data.
  */
 function getFormattedData(text, confidenceThreshold) {
     const bestMatch = getBestMatch(text, confidenceThreshold);
 
+    // Return formatted data if available, else default missing messages:
     return bestMatch === null ? {
         "text": bold(text),
         "match": MISSING_MESSAGE,
@@ -67,47 +95,36 @@ function getFormattedData(text, confidenceThreshold) {
     }
 };
 
-/**
- * Convert text to bold.
- * @date 8/11/2023 - 3:05:43 AM
- *
- * @param {string} text
- * @returns {string} - Bold text.
- */
-function bold(text) {
-    return text !== null && text !== undefined ? `<b>${text}</b>` : text;
-}
+// Event functions:
 
 /**
- * Format answer to readable text.
- * @date 8/11/2023 - 7:00:21 PM
- *
- * @param {boolean} answer - Answer.
- * @returns {string} - Formatted answer.
+ * Update output data when minimum confidence threshold is changed.
+ * @date 8/12/2023 - 3:12:55 AM
  */
-function formatAnswer(answer) {
-    return bold(answer ? "Verdadero" : "Falso");
+function updateMatchThreshold() {
+    const confidenceThreshold = document.getElementById("confidence-threshold").value;
+    document.getElementById("confidence-threshold-value").innerHTML = confidenceThreshold;
+
+    // If there has been any previous input, filter the result:
+    if (lastInput !== null) {
+        const outputData = getFormattedData(lastInput, confidenceThreshold);
+
+        for (let key of KEYS) {
+            document.getElementById(key).innerHTML = outputData[key];
+        }
+    }
 }
 
-/**
- * Format explanation to readable text.
- * @date 8/11/2023 - 7:21:33 PM
- *
- * @param {string} explanation - Explanation. {@link null} if not available.
- * @returns {string} - Formatted explanation.
- */
-function formatExplanation(explanation) {
-    return explanation !== null ? bold(explanation) : MISSING_MESSAGE;
-}
+// Auxiliary functions:
 
 /**
- * Display alert tooltip.
- * @date 8/11/2023 - 7:00:46 PM
+ * Display information tooltip alert.
+ * @date 8/12/2023 - 3:08:04 AM
  *
- * @param {string} field - Field name.
+ * @param {string} elementID - Element ID to show the tooltip for.
  */
-function showTooltip(field) {
-    if (Object.keys(INFO).includes(field)) {
-        alert(INFO[field]);
+function displayTooltip(elementID) {
+    if (Object.keys(INFO).includes(elementID)) {
+        alert(INFO[elementID]);
     }
 }
